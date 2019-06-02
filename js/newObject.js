93acfb590077e2f33ff7fafeded5ee8c7177caec
baranovskyi <baranovskyisasha@gmail.com>
2019-06-02 17:31:03 +0300

    added pagination

--- js/newObject.js
+++ js/newObject.js
@@ -6,7 +6,7 @@ var contentSection = document.getElementById('content');
 var submit = document.getElementById('submit');
 var reader = new FileReader();
 var key = +new Date();
-var catalogBlock;
+
 function newObject (imageUrl, place, square, cost) {
     this.image = imageUrl;
     this.place = place;
@@ -23,8 +23,6 @@ function doThis () {
     cost = costInput.value + "$";
     
     var houseObject = new newObject (imageUrl, place, square, cost);
-  
-    
     localStorage.setItem(key, JSON.stringify(houseObject));
     }
         reader.readAsDataURL(objectImage.files[0]);
@@ -32,9 +30,8 @@ function doThis () {
 for (i = 0; i < localStorage.length; i++) {
 var item = localStorage.getItem(localStorage.key(i));
             item = JSON.parse(item);
-    catalogBlock = document.createElement('div');
+    var catalogBlock = document.createElement('div');
     catalogBlock.className = "catalog-content";
-    
     var img = document.createElement('img');
     img.className = "item-image";
     var place =  document.createElement('p');
@@ -46,6 +43,7 @@ var item = localStorage.getItem(localStorage.key(i));
             img.src =   item['image'];
             place.innerHTML = '<span class="item-bold">' + "Місце: " + "</span>" + item['place'];
             catalogBlock.setAttribute("data-parent", item['place']);
+            catalogBlock.setAttribute("data-num", i + 2)
             square.innerHTML = '<span class="item-bold">' + "Площа: " + "</span>" + item['square']; 
             cost.innerHTML = '<span class="item-bold">' + "Вартість: " + "</span>" + item['cost'];
             catalogBlock.appendChild(img);
@@ -58,81 +56,6 @@ var item = localStorage.getItem(localStorage.key(i));
 
 submit.addEventListener("click", doThis);
 
-var catalogBlocks = document.getElementsByClassName('catalog-content');
-var checkboxes = document.getElementsByClassName("checkBox");
-var qa = [];
-for (let i = 0; i < catalogBlocks.length; i++) {
-    qa.push(catalogBlocks[i].dataset.parent)
-}
-console.log(qa)
-
-
-
-
-
-
-function doThiss (event) {
-    var checkArray = [];
-    for (let i = 0; i < checkboxes.length; i++){
-        if (checkboxes[i].checked) {
-            checkArray.push(checkboxes[i].value)
-            
-        }
-    }
-    console.log(checkArray)
-    for (let j = 0; j < catalogBlocks.length; j++) {
-    if (!checkArray.includes(catalogBlocks[j].dataset.parent)) {
-        catalogBlocks[j].style.display = "none";
-        console.log(catalogBlocks[j].dataset.parent)
-    } else {
-        catalogBlocks[j].style.display = "block";
-    }
-    if (checkArray.length < 1) {
-        catalogBlocks[j].style.display = "block";
-    }
-        // } else {
-        //     catalogBlocks[j].style.display = "none";
-        //     console.log('none')
-        // }
-        // if (event.target.checked != true) {
-        //     catalogBlocks[j].style.display = "block";
-        // }
-    }
-}
-
-
-// function as () {
-//     for (let i = 0; i < checkboxes.length; i++) {
-//         if (checkboxes[i].checked) {
-//             console.log("chek")
-//             for (let j = 0; j < catalogBlocks.length; j++) {
-//                 if (checkboxes[i].value != catalogBlocks[j].dataset.parent) {
-//                     catalogBlocks[j].style.display = "none"
-//                 }   else {
-//                 catalogBlocks[j].style.display = "block"
-//                 console.log('none')
-//             }
-//         }
-//     } 
-//   }
-// }
-  
-
-//     for (let i = 0; i < catalogBlocks.length; i++) {
-//         for (let j = 0; j < checkboxes.length; j++) {
-//             if (checkboxes[j].value == catalogBlocks[i].dataset.parent) {
-//                 catalogBlocks[i].style.display = "block";
-//                 console.log('block')
-//             } else {
-//                 catalogBlocks[i].style.display = "none";
-//                 console.log('none')
-//             }
-//         }
-    
-//         // var item = ;
-//         console.log(catalogBlocks[i].dataset.parent)
-//         // console.log(catalogBlocks.dataset.parent)
-//     }
 
 
 
@@ -140,23 +63,4 @@ function doThiss (event) {
 
 
 
-// for(var i = 0; i < inputs.length; i++) {
-//     if(inputs[i].type == "checkbox") {
-//         inputs[i].checked = true; 
-//     }  
-// }
 
-
-
-
-    // function encodeImageFileAsURL(element) {
-    //     var file = element.files[0];
-    //     var reader = new FileReader();
-    //     reader.onloadend = function() {
-    //      localStorage.setItem("omg", reader.result)
-    //        img.src =   localStorage.getItem("omg")
-
-    //     }
-    //     reader.readAsDataURL(file);
-    //   }
-      
